75ab817613db082cf310e008045e6ee0b62c3e6c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-12 19:36:46 +0800

    fix: padding in list item and mock the style of dropdown temporary

--- src/components/WeatherData/RightDataContainer.tsx
+++ src/components/WeatherData/RightDataContainer.tsx
@@ -2,29 +2,36 @@ const sampleData = [...Array(8)];
 
 const RightDataContainer = () => {
   return (
-    <div className="bg-WEATHER-neutral-800 min-h-full w-[30%] rounded-2xl p-5">
+    <div className="bg-WEATHER-neutral-800 min-h-full w-[34%] rounded-2xl p-5">
       {/* header options */}
       <div className="flex justify-between">
         <p className="text-md font-medium">Hourly forecast</p>
-        <div className="bg-WEATHER-neutral-600 text-WEATHER-neutral-200 rounded-md px-5 py-1 text-sm">
-          Tuesday
+        <div className="bg-WEATHER-neutral-600 text-WEATHER-neutral-200 flex items-center rounded-lg px-5 py-1.5 text-sm">
+          <p>Tuesday</p>
+          <span className="ml-3">
+            <img
+              src="/public/images/icon-dropdown.svg"
+              className="size-2.5"
+              alt=""
+            />
+          </span>
         </div>
       </div>
 
       {/* hourly data  */}
       <div className="mt-4 space-y-3">
         {sampleData.map((_) => (
-          <div className="bg-WEATHER-neutral-700 border-WEATHER-neutral-600 flex w-full items-center justify-between rounded-lg border p-2">
+          <div className="bg-WEATHER-neutral-700 border-WEATHER-neutral-600 flex w-full items-center justify-between rounded-lg border p-2.5">
             <div className="flex items-center gap-2">
               <img
                 src="/public/images/icon-overcast.webp"
-                className="size-8 object-cover"
+                className="size-10 object-cover"
                 alt=""
               />
               <span>{Math.floor(Math.random() * 12)} PM</span>
             </div>
 
-            <p className="mr-2 text-sm">{Math.floor(Math.random() * 60)} °</p>
+            <p className="text-md mr-2">{Math.floor(Math.random() * 60)} °</p>
           </div>
         ))}
       </div>
