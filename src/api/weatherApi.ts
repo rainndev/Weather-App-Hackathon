9e5b00a00a2d33cf62801762672aea7c62d7eb4a
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-13 21:59:19 +0800

    feat: implement fetching data using tanstack query

---
+++ src/api/weatherApi.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://api.open-meteo.com/v1/forecast";
+
+type temperatureType = "fahrenheit" | "celsius";
+
+export const fetchWeather = async (
+  latitude: string,
+  longitude: string,
+  temperature_unit: temperatureType,
+) => {
+  try {
+    const response = await axios.get(API_BASE_URL, {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+        daily: "temperature_2m_max,temperature_2m_min",
+        hourly: "temperature_2m",
+        temperature_unit: temperature_unit,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+};
--- src/components/WeatherData/WeatherDataMain.tsx
+++ src/components/WeatherData/WeatherDataMain.tsx
@@ -1,11 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import LeftDataContainer from "./LeftDataContainer";
 import RightDataContainer from "./RightDataContainer";
+import { fetchWeather } from "@/api/weatherApi";
 
 const WeatherDataMain = () => {
+  const latitude = "15.1505";
+  const longitude = "120.7697";
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["weather", latitude, longitude],
+    queryFn: () => fetchWeather(latitude, longitude, "celsius"),
+    refetchOnWindowFocus: false,
+    staleTime: 300000, //5 mins
+  });
+
+  console.log("data response", data);
+
   return (
     <div className="text-WEATHER-neutral-0 mt-10 flex h-full w-full gap-5">
-      <LeftDataContainer />
-      <RightDataContainer />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <LeftDataContainer />
+          <RightDataContainer />
+        </>
+      )}
     </div>
   );
 };
