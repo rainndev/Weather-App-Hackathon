01caa128dbee4eb1bcb47355bb5d8aa21879410f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-17 23:52:11 +0800

    feat: extend useWeatherData hook to support date range for weather data retrieval

--- src/hooks/useWeatherData.ts
+++ src/hooks/useWeatherData.ts
@@ -1,8 +1,12 @@
-import { fetchWeather } from "@/api/weatherApi";
+import { fetchWeather, fetchWeatherWithDate } from "@/api/weatherApi";
 import { useQuery } from "@tanstack/react-query";
 import { useWeatherLocation } from "./useWeatherLocation";
 
-export const useWeatherData = (city: string) => {
+export const useWeatherData = (
+  city: string,
+  startDate?: string,
+  endDate?: string,
+) => {
   const { locationData, locationPH, currentPHlocation } =
     useWeatherLocation(city);
 
@@ -25,11 +29,39 @@ export const useWeatherData = (city: string) => {
     staleTime: 300000,
   });
 
+  const {
+    data: hourlyDataDate,
+    isLoading: isHourlyDataDateLoading,
+    error: hourlyDataDateError,
+    isError: isHourlyDataDate,
+  } = useQuery({
+    queryKey: ["weather-data-x-date", city, startDate, endDate],
+    queryFn: () => {
+      const latitude = locationPH?.latitude;
+      const longitude = locationPH?.longitude;
+
+      return fetchWeatherWithDate(
+        startDate,
+        endDate,
+        latitude,
+        longitude,
+        "celsius",
+      );
+    },
+    enabled: !!locationData.data?.results?.length,
+    refetchOnWindowFocus: false,
+    staleTime: 300000,
+  });
+
   return {
     weatherData,
     currentPHlocation,
     isLoading,
     error,
     isError,
+    hourlyDataDate,
+    isHourlyDataDateLoading,
+    hourlyDataDateError,
+    isHourlyDataDate,
   };
 };
