7966eade9f21417fccedec3113e15c33d3893624
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-20 22:34:01 +0800

    feat: add loading indicator with message in SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -37,6 +37,19 @@ const SearchBar = () => {
           />
 
           <AnimatePresence>
+            {/* Loading */}
+            {isLoadingLocation && (
+              <div className="bg-WEATHER-neutral-800 border-WEATHER-neutral-700 absolute z-20 mt-2 flex h-fit w-full cursor-pointer items-center gap-3 rounded-lg border p-3 shadow-2xl">
+                <img
+                  src="/public/images/icon-loading.svg"
+                  alt="loading"
+                  className="animate-spin object-cover"
+                />
+                <p className="text-sm">Search in progress</p>
+              </div>
+            )}
+
+            {/* Result  */}
             {(locationData?.results?.length ?? 0) > 0 &&
               !isLoadingLocation &&
               isSelected && (
