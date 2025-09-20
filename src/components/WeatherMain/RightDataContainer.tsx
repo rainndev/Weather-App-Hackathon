871b44e2eccfb1c83f185027c523b131535f916c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-20 17:25:54 +0800

    fix: remove sorting time in hourly forecast

--- src/components/WeatherMain/RightDataContainer.tsx
+++ src/components/WeatherMain/RightDataContainer.tsx
@@ -14,6 +14,7 @@ const RightDataContainer = ({ daily }: RightDataContainerProps) => {
 
   const { hourlyDataDate } = useWeatherData(day, day);
 
+  console.log("sorted data", hourlyDataDate);
   return (
     <div className="bg-WEATHER-neutral-800 scrollbar-hide overflow-y-auto rounded-2xl p-5">
       {/* header options */}
@@ -24,29 +25,27 @@ const RightDataContainer = ({ daily }: RightDataContainerProps) => {
 
       {/* hourly data  */}
       <div className="mt-4 space-y-3">
-        {hourlyDataDate?.hourly?.time
-          ?.sort((a, b) => a - b)
-          .map((data, i) => {
-            //temp
-            const temp = hourlyDataDate.hourly.temperature_2m[i];
-            return (
-              <div
-                key={i}
-                className="bg-WEATHER-neutral-700 border-WEATHER-neutral-600 flex w-full items-center justify-between rounded-lg border p-2.5"
-              >
-                <div className="flex items-center gap-2">
-                  <img
-                    src={getWeatherIcon(temp, "celsius")}
-                    className="size-10 object-cover"
-                    alt=""
-                  />
-                  <span>{convertTo12HrFormat(data)}</span>
-                </div>
-
-                <p className="text-md mr-2">{temp.toFixed(0)} °</p>
+        {hourlyDataDate?.hourly?.time.map((data, i) => {
+          //temp
+          const temp = hourlyDataDate.hourly.temperature_2m[i];
+          return (
+            <div
+              key={i}
+              className="bg-WEATHER-neutral-700 border-WEATHER-neutral-600 flex w-full items-center justify-between rounded-lg border p-2.5"
+            >
+              <div className="flex items-center gap-2">
+                <img
+                  src={getWeatherIcon(temp, "celsius")}
+                  className="size-10 object-cover"
+                  alt=""
+                />
+                <span>{convertTo12HrFormat(data)}</span>
               </div>
-            );
-          })}
+
+              <p className="text-md mr-2">{temp.toFixed(0)} °</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
