80169bb31d381dd31e0d19e2c036f45cefbf770b
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-20 17:38:50 +0800

    feat: enhance unit selection dropdown with hover effects for temperature, wind speed, and precipitation options

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -36,20 +36,24 @@ const Navigation = () => {
               <p className="text-WEATHER-neutral-300 mb-2 text-sm">
                 Temperature
               </p>
-              <h1 className="bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+              <h1 className="hover:bg-WEATHER-neutral-700 bg-WEATHER-neutral-700 -mx-2 mb-1 rounded-md p-2 text-sm">
                 Celsius (°C)
               </h1>
-              <h1 className="-mx-2 p-2 text-sm">Fahrenheit (°F)</h1>
+              <h1 className="hover:bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+                Fahrenheit (°F)
+              </h1>
             </div>
             <div className="bg-WEATHER-neutral-600 -mx-2 my-2 h-px" />
             <div>
               <p className="text-WEATHER-neutral-300 mb-2 text-sm">
                 Wind Speed
               </p>
-              <h1 className="bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+              <h1 className="hover:bg-WEATHER-neutral-700 bg-WEATHER-neutral-700 -mx-2 mb-1 rounded-md p-2 text-sm">
                 km/h
               </h1>
-              <h1 className="-mx-2 p-2 text-sm">mph</h1>
+              <h1 className="hover:bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+                mph
+              </h1>
             </div>
             <div className="bg-WEATHER-neutral-600 -mx-2 my-2 h-px" />
 
@@ -57,10 +61,12 @@ const Navigation = () => {
               <p className="text-WEATHER-neutral-300 mb-2 text-sm">
                 Precipitation
               </p>
-              <h1 className="bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+              <h1 className="hover:bg-WEATHER-neutral-700 bg-WEATHER-neutral-700 -mx-2 mb-1 rounded-md p-2 text-sm">
                 Milimiters (mm)
               </h1>
-              <h1 className="-mx-2 p-2 text-sm">Inches (in)</h1>
+              <h1 className="hover:bg-WEATHER-neutral-700 -mx-2 rounded-md p-2 text-sm">
+                Inches (in)
+              </h1>
             </div>
           </div>
         )}
